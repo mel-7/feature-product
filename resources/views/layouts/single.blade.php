36f27d6c83b6f07e868254582f84b5ad7dc91cee
moikzz214 <moikzz214@gmail.com>
2020-09-17 12:13:30 +0400

    Add 360 label

--- resources/views/layouts/single.blade.php
+++ resources/views/layouts/single.blade.php
@@ -120,9 +120,86 @@
             animation: cd-pulse 2s infinite;
         }
  
-       .icon-360 {position: absolute;
-    top: 0;
-    margin: 30px;} 
+.icon-360 {position: absolute;
+top: 0;
+margin: 15px;
+} 
+
+.icon-360 img{ width: 50%; }
+
+.round-container{position: absolute;
+    left: 40%;}
+.center-con {
+  width: 100%;
+    bottom: 65px;
+    height: 50px;
+    position: absolute;
+    padding-top: 7px;
+    background: rgb(14 14 14 / 50%);
+} 
+
+.round {
+  border: 2px solid #fff;
+    width: 35px;
+    display: inline-block;
+    height: 35px;
+    border-radius: 100%;
+} 
+.round-label{
+  display: inline-block;
+    color: #fff;
+    bottom: 10px;
+    position: relative;
+    font-weight: bold;
+    margin-right:10px;
+}
+
+#cta{
+    width:100%; cursor: pointer; position: absolute;
+}
+
+#cta .arrow{left: 3%;}
+.arrow {position: absolute; bottom: 0;  margin-left:0px; width: 12px; height: 12px; background-size: contain; top:10px;}
+.segunda{margin-left: 8px;}
+.next-arrow {
+	background-image: url(data:image/svg+xml;base64,PHN2ZyBpZD0iTGF5ZXJfMSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB2aWV3Qm94PSIwIDAgNTEyIDUxMiI+PHN0eWxlPi5zdDB7ZmlsbDojZmZmfTwvc3R5bGU+PHBhdGggY2xhc3M9InN0MCIgZD0iTTMxOS4xIDIxN2MyMC4yIDIwLjIgMTkuOSA1My4yLS42IDczLjdzLTUzLjUgMjAuOC03My43LjZsLTE5MC0xOTBjLTIwLjEtMjAuMi0xOS44LTUzLjIuNy03My43UzEwOSA2LjggMTI5LjEgMjdsMTkwIDE5MHoiLz48cGF0aCBjbGFzcz0ic3QwIiBkPSJNMzE5LjEgMjkwLjVjMjAuMi0yMC4yIDE5LjktNTMuMi0uNi03My43cy01My41LTIwLjgtNzMuNy0uNmwtMTkwIDE5MGMtMjAuMiAyMC4yLTE5LjkgNTMuMi42IDczLjdzNTMuNSAyMC44IDczLjcuNmwxOTAtMTkweiIvPjwvc3ZnPg==);
+}
+
+@keyframes bounceAlpha {
+  0% {opacity: 1; transform: translateX(0px) scale(1);}
+  25%{opacity: 0; transform:translateX(10px) scale(0.9);}
+  26%{opacity: 0; transform:translateX(-10px) scale(0.9);}
+  55% {opacity: 1; transform: translateX(0px) scale(1);}
+}
+
+.bounceAlpha {
+    animation-name: bounceAlpha;
+    animation-duration:1.4s;
+    animation-iteration-count:infinite;
+    animation-timing-function:linear;
+}
+
+.arrow.primera.bounceAlpha {
+    animation-name: bounceAlpha;
+    animation-duration:1.4s;
+    animation-delay:0.2s;
+    animation-iteration-count:infinite;
+    animation-timing-function:linear;
+}
+
+.round .arrow{
+    animation-name: bounceAlpha;
+    animation-duration:1.4s;
+    animation-iteration-count:infinite;
+    animation-timing-function:linear;
+}
+.round .arrow.primera{
+    animation-name: bounceAlpha;
+    animation-duration:1.4s;
+    animation-delay:0.2s;
+    animation-iteration-count:infinite;
+    animation-timing-function:linear;
+}
 
 @-webkit-keyframes cd-pulse {
   0% {
@@ -338,6 +415,12 @@
     .container {
         height: 275px;
     }
+
+    .center-con { bottom: 30px; height: 40px; } 
+    .round-container{ left: 30%; }
+    .round-label{ font-size:12px;}
+    .round{ width: 25px; height: 25px; }
+    .arrow{ top:7px;width: 8px; height: 8px;}
 }
     </style>
 <script> var base_url = "{{URL::to('/')}}"; </script>
